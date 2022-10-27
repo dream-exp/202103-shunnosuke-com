895fa8e73ae6990343ab9485b48ec00882722d18
Shunnosuke Kataoka <shunnosuke834@gmail.com>
2022-10-27 20:40:17 +0900

    fix documenttsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,34 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { DocumentContext } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-type Props = {};
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-class Document extends NextDocument<Props> {
-  render() {
-    return (
-      <Html lang="ja">
-        <Head />
-        <body>
-          <script></script> 
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
+    try {
+      ctx.renderPage = () => {
+        return originalRenderPage({
+          enhanceApp: (App) =>
+            function MyDocument(props) {
+              return sheet.collectStyles(<App {...props} />);
+            }
+        });
+      };
+
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 }
-
-export default Document;
